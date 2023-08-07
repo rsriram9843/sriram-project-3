2a922976dcf3a700985f56718150d18ee3a87e34
SRIRAM R <141619479+rsriram9843@users.noreply.github.com>
2023-08-07 14:47:48 +0530

    Add files via upload

---
+++ LoginBean.java
@@ -0,0 +1,39 @@
+package com.login.bean;
+
+public class LoginBean {
+public Integer userId;
+public String username;
+public String password;
+public String name;
+public String rolln0;
+public Integer getUserId() {
+	return userId;
+}
+public void setUserId(Integer userId) {
+	this.userId = userId;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getRolln0() {
+	return rolln0;
+}
+public void setRolln0(String rolln0) {
+	this.rolln0 = rolln0;
+}
+}
